45389e1835d4c7872e6f103d3c7d2d91922cbf1d
Annie Zhou <zhoua@zhoua-macbookpro2.roam.corp.google.com>
2019-03-29 20:11:49 -0700

    new style starts

---
+++ index.js
@@ -0,0 +1,19 @@
+var app = new Vue({
+  el: '#top-nav',
+  data: {
+    labels: [
+      { 
+        text: 'About',
+        link: 'https://www.google.com' 
+      },
+      { 
+        text: 'Portfolio',
+        link: '' 
+      },
+      { 
+        text: 'Journal',
+        link: '' 
+      }
+    ]
+  }
+})
